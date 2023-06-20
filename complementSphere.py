b72d192f3c6aed176362849468416b2464178157
allineibraimo <89539309+allineibraimo@users.noreply.github.com>
2023-06-20 16:01:40 -0500

    Update complementSphere.py
    added comments

--- complementSphere.py
+++ complementSphere.py
@@ -1,7 +1,10 @@
+##this code serves as the basis for getting the complement of two gcodes
+##start by getting the complement of two spheres given the radius and points in the surface of the sphere
 import numpy as np
 import math 
 
-
+##This function takes in 3 arrays points, of sphere 1, 2 and the radius, then proceeds to calculate the volumes of each sphere by using the volume
+##of sphere function (vol = 4/3 * pi * radius^3)
 def complementFunc(sphereOne, sphereTwo, radius):
     radiusSphereOne = math.sqrt((sphereOne[0] - radius[0]) ** 2 + (sphereOne[1] - radius[1]) ** 2 + (sphereOne[2] - radius[2]) ** 2)
     radiusSphereTwo = math.sqrt((sphereTwo[0] - radius[0]) ** 2 + (sphereTwo[1] - radius[1]) ** 2 + (sphereTwo[2] - radius[2]) ** 2)
