18565193345732637394bc16c59600a6ec17ed24
allineibraimo <89539309+allineibraimo@users.noreply.github.com>
2023-09-08 16:28:51 -0500

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -12,15 +12,35 @@ driver.get("https://www.thingiverse.com/")
 # WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@id="sp_message_iframe_382445"]')))
 # WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//button[contains(@title,'ACCEPT ALL')]"))).click()
 
-time.sleep(600)
+time.sleep(4)
 
-driver.find_element(By.CLASS_NAME, "ThingCardBody__cardBodyWrapper--BLLzJ").click()
-time.sleep(5)
-driver.find_element(By.CLASS_NAME, "DonwloadButton").click()
-time.sleep(15)
-driver.back()
+driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
+time.sleep(2)
+
+stls = driver.find_elements(By.CLASS_NAME, "ThingCard__thingCard--fG91n")
+
+time.sleep(4)
+
+for link in stls:
+    link.click()
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//button[@class= "Button__button--xv8c4 Button__primary--grPsm Button__left--Zsp9y Button__md--hfp1W Button__icon--UCU2X"]').click()
+    time.sleep(10)
+    driver.find_element(By.CLASS_NAME, "Modal__closeButton--GTlJu").click()
+    time.sleep(3)
+    driver.back()
+    time.sleep(4)
+
+# driver.find_element(By.CLASS_NAME, "ThingCardBody__cardBodyWrapper--BLLzJ").click()
+# time.sleep(6)
+# driver.find_element(By.XPATH, '//button[@class= "Button__button--xv8c4 Button__primary--grPsm Button__left--Zsp9y Button__md--hfp1W Button__icon--UCU2X"]').click()
+# time.sleep(10)
+# driver.find_element(By.CLASS_NAME, "Modal__closeButton--GTlJu").click()
+# time.sleep(3)
+# driver.back()
+# time.sleep(4)
 
 
-
+# <button class="Button__button--xv8c4 Button__primary--grPsm Button__left--Zsp9y Button__md--hfp1W Button__icon--UCU2X"><img class="" src="https://cdn.thingiverse.com/site/assets/inline-icons/1f6b7330c1fc454b22fe.svg" alt="DownloadButton" loading="lazy">Download All Files</button>
 
 driver.quit()
